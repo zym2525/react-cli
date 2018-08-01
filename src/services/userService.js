66db0717f1531080f72edeb2df1e968c0359e56c
zhengyiming <540361168@qq.com>
2018-08-01 15:45:04 +0800

    111

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import dataApi from '../constants/api';
 import * as requestService from './request';
 
-export function login (userdata, success, failed) {
+export function login(userdata, success, failed) {
   let fetchApi = dataApi.user.auth;
   let data = {
     LoginName: userdata.username,
@@ -15,7 +15,18 @@ export function login (userdata, success, failed) {
   return requestService.fetchPost(fetchApi, data, headers, success, failed);
 }
 
-export function getSceneInfo (data, success, failed) {
+export function getSceneInfo(data, success, failed) {
   return requestService.fetchGet('http://localhost:3000/api/getSceneInfo', {}, success, failed);
 }
 
+export function getList(data, success, failed) {
+  return requestService.fetchGet('http://localhost:3000/api/getList', {}, success, failed);
+}
+
+export function promiseAll(params1, params2) {
+  console.log(123)
+  return Promise.all([getSceneInfo(), getList()]).then(([d1, d2]) => {
+    console.log(d1, d2)
+    return [d1, d2];
+  })
+}
